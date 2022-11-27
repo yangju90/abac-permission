7dd22db6350212acc7cfd06e14a20829705828fa
Mat <yangju90@163.com>
2022-11-27 22:43:47 +0800

    Update schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -15,3 +15,13 @@ CREATE TABLE `role` (
   UNIQUE KEY `ix_role_name` (`name`),
   KEY `ix_role_path_string` (`path_string`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Role';
+
+
+
+INSERT INTO `role` (name,node,path_string,status,deleted,in_user,in_date,last_edit_user,last_edit_date) VALUES ('root',1,'1',1,0,'admin',1658902386540,NULL,NULL);
+INSERT INTO `role` (name,node,path_string,status,deleted,in_user,in_date,last_edit_user,last_edit_date) VALUES ('Client',2,'1_2',1,0,'admin',1658902386540,NULL,NULL);
+INSERT INTO `role` (name,node,path_string,status,deleted,in_user,in_date,last_edit_user,last_edit_date) VALUES ('Appt. Mgr.',3,'1_3',1,0,'admin',1658902386540,NULL,NULL);
+INSERT INTO `role` (name,node,path_string,status,deleted,in_user,in_date,last_edit_user,last_edit_date) VALUES ('Warehouse Mgr.',4,'1_4',1,0,'admin',1658902386540,NULL,NULL);
+INSERT INTO `role` (name,node,path_string,status,deleted,in_user,in_date,last_edit_user,last_edit_date) VALUES ('Appt_Internal',5,'1_3_5',1,0,'admin',1658902386540,NULL,NULL);
+INSERT INTO `role` (name,node,path_string,status,deleted,in_user,in_date,last_edit_user,last_edit_date) VALUES ('Warehouse_Internal',6,'1_4_6',1,0,'admin',1658902386540,NULL,NULL);
+INSERT INTO `role` (name,node,path_string,status,deleted,in_user,in_date,last_edit_user,last_edit_date) VALUES ('Visitor',7,'1_2_7',1,0,'admin',1658902386540,NULL,NULL);
